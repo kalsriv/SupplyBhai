999510ab74b12ba0c3ef4fd7525a632a79831a5d
Kalyan Srivastava <kalsriv@gmail.com>
2026-01-05 03:37:39 +0000

    After testing custom auth function 1

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 import streamlit as st
 from rag_helper_utility_push import process_document_to_chroma_db, answer_question
-
-from st_paywall import add_auth
 import streamlit as st
 
 from auth import require_subscription
