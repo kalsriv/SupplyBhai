03a20b798f8fdfa6a0658767dad7d9d75eb7e5a2
Kalyan Srivastava <kalsriv@gmail.com>
2026-01-07 03:24:21 +0000

    After llm step 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+from langchain_groq import ChatGroq
 from rag_helper_utility_push import process_document_to_chroma_db, answer_question
 from auth import require_subscription, create_customer_portal, logout
 
